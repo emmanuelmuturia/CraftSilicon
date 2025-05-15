bb0f2f1a02e8c9dd067bc07f6ffcbbf8f3b4c04a
Emmanuel Muturia <science@emmanuelmuturia.com>
2025-05-15 11:34:02 +0300

    Format the code...

--- app/src/main/java/emmanuelmuturia/craftsilicon/home/ui/state/HomeScreenUIState.kt
+++ app/src/main/java/emmanuelmuturia/craftsilicon/home/ui/state/HomeScreenUIState.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Craft Silicon
+ *
+ * Licenced under the Apache License, Version 2.0 (the "Licence");
+ * you may not use this file except in compliance with the Licence.
+ * You may obtain a copy of the Licence at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
 package emmanuelmuturia.craftsilicon.home.ui.state
 
 import emmanuelmuturia.craftsilicon.home.data.model.CityWeather
@@ -6,5 +21,5 @@ data class HomeScreenUIState(
     val isLoading: Boolean = false,
     val cityWeather: CityWeather? = null,
     val error: String? = null,
-    val cityName: String = "Nairobi"
+    val cityName: String = "Nairobi",
 )
--- app/src/main/java/emmanuelmuturia/craftsilicon/home/ui/viewmodel/HomeScreenViewModel.kt
+++ app/src/main/java/emmanuelmuturia/craftsilicon/home/ui/viewmodel/HomeScreenViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Craft Silicon
+ *
+ * Licenced under the Apache License, Version 2.0 (the "Licence");
+ * you may not use this file except in compliance with the Licence.
+ * You may obtain a copy of the Licence at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
 package emmanuelmuturia.craftsilicon.home.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
@@ -11,9 +26,8 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class HomeScreenViewModel(
-    private val homeRepository: HomeRepository
-): ViewModel() {
-
+    private val homeRepository: HomeRepository,
+) : ViewModel() {
     val homeScreenUIState = MutableStateFlow(value = HomeScreenUIState())
 
     init {
@@ -23,15 +37,14 @@ class HomeScreenViewModel(
     private fun getCityWeather(city: String) {
         homeScreenUIState.value = HomeScreenUIState(isLoading = true)
         viewModelScope.launch {
-            homeRepository.getCityWeather(city = city).asResult().collect{ result ->
+            homeRepository.getCityWeather(city = city).asResult().collect { result ->
 
-                when(result) {
-
+                when (result) {
                     is CraftSiliconNetworkResult.Success -> {
                         homeScreenUIState.update {
                             it.copy(
                                 isLoading = false,
-                                cityWeather = result.data
+                                cityWeather = result.data,
                             )
                         }
                     }
@@ -40,15 +53,12 @@ class HomeScreenViewModel(
                         homeScreenUIState.update {
                             it.copy(
                                 isLoading = false,
-                                error = result.error
+                                error = result.error,
                             )
                         }
                     }
-
                 }
-
             }
         }
     }
-
 }
